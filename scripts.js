c9b47db77f9597d4299c0a2d1107da26937f38bf
Jared Gaston <jaredgaston68@gmail.com>
2019-08-03 20:13:03 -0500

    get checkers to load onto board

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
 function newGame() {
-    alert("let it begin");
+    initializeBoard();
+}
+
+function initializeBoard() {
+    initializeRedSquares();
+    initializeWhiteSquares();
+}
+
+function initializeWhiteSquares() {
+    var squares = document.querySelectorAll(".white-square");
+    squares.forEach(square => {
+        square.appendChild(createChecker("white"));
+    });
+}
+
+function initializeRedSquares() {
+    var squares = document.querySelectorAll(".red-square");
+    squares.forEach(square => {
+        var checker = createChecker("red");
+        square.appendChild(checker);
+    });
+}
+
+function createChecker(colorClass) {
+    var checker = document
+        .createElement("div");
+    checker.classList.add("checker");
+    checker.classList.add(colorClass);
+    return checker;
 }
